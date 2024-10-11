e9da819981976c5d7dff4a6f16f9ecd79f902aa9
Redian16 <redian5283@gmail.com>
2024-10-11 09:14:13 +0700

    Add files via upload

---
+++ tipe data list.py
@@ -0,0 +1,31 @@
+#membuat kumoulan sring (nama nama buah)
+# list kosong
+list_kosong = []
+
+# list yang berisi kumpulan string
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+#scling list
+#print(list_buah [-1])
+#print(list_buah [-2])
+#print(list_buah [-3])
+#print(list_buah [-4])
+
+#list_buah[0] = 'mangga'
+#print(list_buah)
+
+#tambah data baru
+#list_buah.append('Sirsak')
+#print(list_buah)
+
+# tambah data di awal list
+#list_buah.insert(3, 'alpukat')
+#print(list_buah)
+
+# hapus item pertama dengan nilai 'Jambu'
+list_buah = ['Mangga', 'Jambu', 'Jeruk', 'Durian']
+print(list_buah)
+
+# hapus item pertama dengan nilai 'Jambu'
+list_buah.remove('Jambu')
+
+print(list_buah)
